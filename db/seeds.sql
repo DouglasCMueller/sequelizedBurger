fbef2d085cb028b9dcf55c6f101c31fc18d50b58
Douglas Mueller <douglasmueller6@gmail.com>
2020-02-05 12:58:45 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon BBQ Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder with Cheese', false);
